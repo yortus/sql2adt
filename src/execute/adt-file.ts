8647f51ac1d70c07647f473677baf28cca0a8272
yortus <yortus@gmail.com>
2018-08-23 15:35:09 +0800

    back out case-insensitive column name comparisons

--- src/execute/adt-file.ts
+++ src/execute/adt-file.ts
@@ -49,7 +49,7 @@ export class AdtFile {
      * NB2: deleted records are skipped, so the number of records returned may be less than limit / recordCount.
      * @param options.offset number of records to skip before reading records. Default: 0
      * @param options.limit number of records to read before returning. Default: <record count - offset>
-     * @param options.columnNames subset of column names to return (case-insensitive). Default: <all column names>
+     * @param options.columnNames subset of column names to return. Default: <all column names>
      */
      async fetchRecords(options?: {offset?: number, limit?: number, columnNames?: string[]}) {
         options = options || {};
@@ -66,10 +66,9 @@ export class AdtFile {
         // Calculate column name whitelist. Compare options.columnNames case-insensitively.
         let columns = this.columns;
         if (options.columnNames) {
-            let whitelist = options.columnNames;
-            let invalidNames = whitelist.filter(wn => !columns.some(c => c.name.toLowerCase() === wn.toLowerCase()));
+            let invalidNames = options.columnNames.filter(wn => !columns.some(c => c.name === wn));
             if (invalidNames.length > 0) throw new Error(`Invalid column name(s): ${invalidNames}`);
-            columns = whitelist.map(wn => columns.find(col => col.name.toLowerCase() === wn.toLowerCase())!);
+            columns = options.columnNames.map(wn => columns.find(col => col.name === wn)!);
         }
 
         // Read all fetched records into a single buffer.
